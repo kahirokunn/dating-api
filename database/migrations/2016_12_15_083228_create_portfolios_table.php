6e1532d34fe96a3b426cf3163a42032dd3a83734
k-okina <okinakahiro@gmail.com>
2016-12-15 17:44:24 +0900

    Make portfolios table

---
+++ database/migrations/2016_12_15_083228_create_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('image_url')->nullable();
+            $table->text('base_image_url')->nullable();
+            $table->timestamps();
+
+            // usersテーブルのレコードが削除されたら
+            // 削除されたレコードに関係するレコードを一緒に削除する
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
