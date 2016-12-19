c919866ed37000b0ca203160b60cf6fd0fe48b10
k-okina <okinakahiro@gmail.com>
2016-12-19 16:53:04 +0900

    Update ProfileDetail model

--- app/Model/ProfileDetail.php
+++ app/Model/ProfileDetail.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProfileDetail extends Model
 {
-    protected $table = "profile_details";
+    protected $table = 'profile_details';
+
+    protected function user()
+    {
+        return $this->belongsTo('App\Model\User');
+    }
 }
