f11846f097e298938e05256e4f1596d2521383c6
k-okina <okinakahiro@gmail.com>
2016-12-19 16:52:30 +0900

    Update Portfolio model

--- app/Model/Portfolio.php
+++ app/Model/Portfolio.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    protected $table = "portfolios";
+    protected $table = 'portfolios';
+
+    protected function user()
+    {
+        return $this->belongsTo('App\Model\User');
+    }
 }
