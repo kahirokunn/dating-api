7452d1f3be1f2cd42f2b8dfdff17a1015d503678
k-okina <okinakahiro@gmail.com>
2016-12-19 16:29:21 +0900

    Update Chatroom model

--- app/Model/Chatroom.php
+++ app/Model/Chatroom.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chatroom extends Model
 {
-    protected $table = "chatrooms";
+    protected $table = 'chatrooms';
+
+    protected function messages()
+    {
+        return $this->hasMany('App\Model\ChatroomMessage');
+    }
+
+    protected function users()
+    {
+        return $this->hasMany('App\Model\User');
+    }
 }
