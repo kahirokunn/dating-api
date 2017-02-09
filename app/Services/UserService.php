9847015ddd7a71b53304183e81557431a3d787e0
k-okina <okinakahiro@gmail.com>
2017-02-09 09:04:05 +0900

    Update User Service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Services;
 
@@ -9,25 +9,46 @@ use App\Models\User;
 final class UserService
 {
 
-    public function getByType(string $type, $value, array $field = ['*']) : array
+    /**
+     * get user parameters by type
+     * you can select get parameters what you want
+     * default is all parameters
+     *
+     * @param string $type
+     * @param $value
+     * @param array $field
+     * @return array
+     */
+    public function getByType(string $type, $value, array $field = ['*']): array
     {
         switch ($type) {
             case 'id':
                 return $this->getById($value, $field);
+
             case 'email':
                 return $this->getByEmail($value, $field);
+
             case 'facebook':
                 return $this->getByFacebookId($value, $field);
+
             case 'twitter':
                 return $this->getByTwitterId($value, $field);
+
             case 'instagram':
                 return $this->getByInstagramId($value, $field);
+
             case 'tumblr_id':
                 return $this->getByTumblrId($value, $field);
         }
+        return [];
     }
 
-    public function ifExistsByType(string $type, $value) : bool
+    /**
+     * @param string $type id or email or name
+     * @param $value
+     * @return bool
+     */
+    public function ifExistsByType(string $type, $value): bool
     {
         switch ($type) {
             case 'id':
@@ -39,52 +60,51 @@ final class UserService
             case 'name':
                 return $this->ifExistsByName($value);
         }
+        return false;
     }
 
-
-    private function ifExistsById(int $id) : bool
+    private function ifExistsById(int $id): bool
     {
-        return (bool)User::where("id", "=", $id)->count();
+        return User::where("id", "=", $id)->exists();
     }
 
-    private function ifExistsByEmail(string $email) : bool
+    private function ifExistsByEmail(string $email): bool
     {
-        return (bool)User::where("email", "=", $email)->count();
+        return User::where("email", "=", $email)->exists();
     }
 
-    private function ifExistsByName(string $name) : bool
+    private function ifExistsByName(string $name): bool
     {
-        return (bool)User::where("name", "=", $name)->count();
+        return User::where("name", "=", $name)->exists();
     }
 
-
-    private function getById(int $id, array $field = ['*']) : array
+    private function getById(int $id, array $field = ['*']): array
     {
         return User::select($field)->where("id", "=", $id)->first()->toArray();
     }
 
-    private function getByEmail(string $email, array $field = ['*']) : array
+    private function getByEmail(string $email, array $field = ['*']): array
     {
-        return User::select($field)->where("email", "=", $email)->first();
+        return User::select($field)->where("email", "=", $email)->first()->toArray();
     }
 
-    private function getByFacebookId(int $facebook_id, array $field = ['*']) : array
+    private function getByFacebookId(int $facebook_id, array $field = ['*']): array
     {
-        return User::select($field)->where("facebook_id", "=", $facebook_id)->first();
+        return User::select($field)->where("facebook_id", "=", $facebook_id)->first()->toArray();
     }
 
-    private function getByTwitterId(int $twitter_id, array $field = ['*']) : array
+    private function getByTwitterId(int $twitter_id, array $field = ['*']): array
     {
-        return User::select($field)->where("twitter_id", "=", $twitter_id)->first();
+        return User::select($field)->where("twitter_id", "=", $twitter_id)->first()->toArray();
     }
 
-    private function getByInstagramId(int $instagram_id, array $field = ['*']) : array
+    private function getByInstagramId(int $instagram_id, array $field = ['*']): array
     {
-        return User::select($field)->where("twitter_id", "=", $instagram_id)->first();
+        return User::select($field)->where("instagram_id", "=", $instagram_id)->first()->toArray();
     }
 
-    private function getByTumblrId(int $tumblr_id, array $field = ['*']) : array
+    private function getByTumblrId(int $tumblr_id, array $field = ['*']): array
     {
-        return User::select($field)->where("twitter_id", "=", $tumblr_id)->first();
+        return User::select($field)->where("tumblr_id", "=", $tumblr_id)->first()->toArray();
     }
 }
